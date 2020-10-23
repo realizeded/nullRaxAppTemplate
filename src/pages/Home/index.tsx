6241399d84f7f56ba867e30000ed80ded5ea507c
lianjun <lianjun>
2020-10-23 16:20:31 +0800

    app

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import { createElement } from 'rax';
+import View from 'rax-view';
+import Text from 'rax-text';
+
+import './index.css';
+import Logo from '../../components/Logo';
+
+export default function Home() {
+  return (
+    <View className="home">
+      <button>1212</button>
+    </View>
+  );
+}
